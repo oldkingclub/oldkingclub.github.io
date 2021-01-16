c3b0e15fbab829ca17d7b6a0625269f73f209c35
oldkingclub <yicheng751@foxmail.com>
2021-01-16 21:21:22 +0800

    update

--- js/chunk-2d0e1b35.35c2f9d7.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e1b35"],{"7c0c":function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("v-app",[t("router-view")],1)},a=[],u={name:"Blanklayout",components:{}},l=u,o=t("c701"),p=t("7033"),r=t.n(p),s=t("05f7"),i=Object(o["a"])(l,c,a,!1,null,null,null);e["default"]=i.exports;r()(i,{VApp:s["a"]})}}]);
-//# sourceMappingURL=chunk-2d0e1b35.35c2f9d7.js.map
